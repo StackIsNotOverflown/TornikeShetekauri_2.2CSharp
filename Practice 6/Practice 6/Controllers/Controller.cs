a9daea6dff665371f8f67a8fc8f6e13942b47541
Tornike Shetekauri <tornike.shetekauri1@gmail.com>
2025-02-24 19:56:07 +0400

    "first commit"

---
+++ Practice 6/Practice 6/Controllers/Controller.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using static Practice_6.Model_events;
+
+namespace Practice_6.Controllers
+{
+    [ApiController]
+    [Route("api/events")]
+    public class EventsController : ControllerBase
+    {
+        private static List<Event> _events = new List<Event>();
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadJsonFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("Invalid file.");
+            }
+
+            using (var stream = new StreamReader(file.OpenReadStream()))
+            {
+                var json = await stream.ReadToEndAsync();
+                var jsonData = JsonConvert.DeserializeObject<dynamic>(json);
+                _events = JsonConvert.DeserializeObject<List<Event>>(jsonData["events"].ToString());
+            }
+
+            return Ok("File processed successfully.");
+        }
+
+        [HttpGet("customers")]
+        public IActionResult GetCustomers()
+        {
+            var customers = _events
+                .SelectMany(e => new[] { e.SrcNumber, e.DstNumber })
+                .Distinct()
+                .Select(num => new Customer { PhoneNumber = num })
+                .ToList();
+
+            return Ok(customers);
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetEvents()
+        {
+            return Ok(_events);
+        }
+    }
+}
---
+++ Practice 6/Practice 6/Model events.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice_6
+{
+    public class Model_events
+    {
+        public class Event
+        {
+            public string Type { get; set; }
+            public string SrcNumber { get; set; }
+            public string DstNumber { get; set; }
+            public DateTime Time { get; set; }
+            public int? Duration { get; set; }
+        }
+
+        public class Customer
+        {
+            public string PhoneNumber { get; set; }
+        }
+   
+
+    }
+}

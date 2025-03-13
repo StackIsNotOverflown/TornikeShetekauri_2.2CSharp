b852518c654c12c0195c1310e238cdd13ea1237f
Tornike Shetekauri <tornike.shetekauri1@gmail.com>
2025-03-13 18:31:46 +0400

    "commit"

--- Practice 6/Practice 6/Controllers/Controller.cs
+++ Practice 6/Practice 6/Controllers/Controller.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using OfficeOpenXml;
+using System.IO;
 using static Practice_6.Model_events;
 
 namespace Practice_6.Controllers
@@ -44,6 +46,40 @@ namespace Practice_6.Controllers
         public IActionResult GetEvents()
         {
             return Ok(_events);
+        }
+
+        [HttpGet("download")]
+        public IActionResult DownloadEventsAsExcel()
+        {
+            // Create a new Excel package
+            using (var package = new ExcelPackage())
+            {
+                // Create a worksheet
+                var worksheet = package.Workbook.Worksheets.Add("Events");
+
+                // Add headers to the worksheet
+                worksheet.Cells[1, 1].Value = "Type";
+                worksheet.Cells[1, 2].Value = "SrcNumber";
+                worksheet.Cells[1, 3].Value = "DstNumber";
+                worksheet.Cells[1, 4].Value = "Time";
+                worksheet.Cells[1, 5].Value = "Duration";
+
+                // Populate the worksheet with event data
+                int row = 2;
+                foreach (var eventItem in _events)
+                {
+                    worksheet.Cells[row, 1].Value = eventItem.Type;
+                    worksheet.Cells[row, 2].Value = eventItem.SrcNumber;
+                    worksheet.Cells[row, 3].Value = eventItem.DstNumber;
+                    worksheet.Cells[row, 4].Value = eventItem.Time.ToString("yyyy-MM-dd HH:mm:ss");
+                    worksheet.Cells[row, 5].Value = eventItem.Duration ?? (object)"N/A";  // Handle null Duration
+                    row++;
+                }
+
+                // Set the content type and file name
+                var excelFile = package.GetAsByteArray();
+                return File(excelFile, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "events.xlsx");
+            }
         }
     }
 }

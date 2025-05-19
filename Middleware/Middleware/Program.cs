7ab47a6621785bd24397149b79d053c451604f12
Tornike Shetekauri <tornike.shetekauri1@gmail.com>
2025-05-19 20:23:08 +0400

    "Middleware"

---
+++ Middleware/Middleware/Program.cs
@@ -0,0 +1,28 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/" || context.Request.Path == "/image")
+    {
+        var imagePath = Path.Combine(Directory.GetCurrentDirectory(), "MyFolder", "Screenshot (44).png");
+
+        if (File.Exists(imagePath))
+        {
+            context.Response.ContentType = "image/Png"; 
+            var imageBytes = await File.ReadAllBytesAsync(imagePath);
+            await context.Response.Body.WriteAsync(imageBytes);
+            return;
+        }
+        else
+        {
+            context.Response.StatusCode = 404;
+            await context.Response.WriteAsync("სახელი შეცვალე");
+            return;
+        }
+    }
+
+    await next();
+});
+
+app.Run(); 

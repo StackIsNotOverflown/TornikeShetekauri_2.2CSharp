907063bd242d2af4d2fde99d8a51b7eccaa28f24
Tornike Shetekauri <tornike.shetekauri1@gmail.com>
2025-06-23 14:06:27 +0400

    რატომღაც წაშლის შემდეგ ფოლდერის გარეთ დარჩა ფაილები

--- GreenSchoolCAT/TestViewModel.cs
+++
@@ -1,7 +0,0 @@
-public class TestViewModel
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public int QuestionCount { get; set; }
-    public int ResultCount { get; set; }
-}

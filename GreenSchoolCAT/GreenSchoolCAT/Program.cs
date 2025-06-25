bf90970e22ef7f3d49221c4d08e740e472d94c8c
Tornike Shetekauri <tornike.shetekauri1@gmail.com>
2025-06-25 21:36:37 +0400

    Optimzations for the bad server coming from the free tier and might not work but still optimizations

--- GreenSchoolCAT/GreenSchoolCAT/Program.cs
+++ GreenSchoolCAT/GreenSchoolCAT/Program.cs
@@ -5,32 +5,60 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(opts =>
-    opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<ICatService, CatService>();
-builder.Services.AddControllersWithViews();
+builder.WebHost.UseSetting("preventHostingStartup", "true"); 
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.Limits.MaxRequestBodySize = 10 * 1024 * 1024; 
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>(opts =>
+    opts.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        o => o.EnableRetryOnFailure(
+            maxRetryCount: 3,
+            maxRetryDelay: TimeSpan.FromSeconds(5),
+            errorNumbersToAdd: null)));
+
+
+builder.Services.AddControllersWithViews()
+    .AddViewOptions(options =>
+    {
+        options.HtmlHelperOptions.ClientValidationEnabled = false; 
+    });
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(opts =>
     {
+        opts.Cookie.Name = "CAT.Auth"; 
         opts.LoginPath = "/Account/Login";
         opts.AccessDeniedPath = "/Account/Login";
-        opts.ExpireTimeSpan = TimeSpan.FromHours(1);
+        opts.ExpireTimeSpan = TimeSpan.FromMinutes(30); 
+        opts.SlidingExpiration = true;
     });
 
-builder.Services.AddAuthorization();
+ThreadPool.SetMinThreads(4, 4);
+ThreadPool.SetMaxThreads(8, 8);
 
 var app = builder.Build();
 
+app.UseResponseCompression(); 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers["Cache-Control"] = "public,max-age=3600"; // Better caching
+    }
+});
+
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.Logger.LogInformation("Application configured for Free Tier");
 app.Run();
